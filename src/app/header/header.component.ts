c40f9c3b3f7d58f95e93a7f44c48b885e8606251
Oleg Wenner <aleg1691@gmail.com>
2024-11-07 01:50:22 +0100

    Responsive

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -18,31 +18,29 @@ export class HeaderComponent {
 
   menuOpen = false;
 
-  // Toggle the burger menu
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
   }
 
-  // Close the menu
   closeMenu() {
     this.menuOpen = false;
   }
 
-  // Handle language change and close the menu after language switch
   changeLanguage(lang: string) {
-    // Set the language using the translation service
     this.translate.setLanguage(lang);
-    this.closeMenu(); // Close the menu after language change
+    this.closeMenu();
   }
 
-  // Close menu when clicked inside the nav but not on a link
   closeMenuOnClickInside(event: Event) {
     const target = event.target as HTMLElement;
     const isLink = target.tagName === 'A';
 
-    // If the target is not a link, close the menu
     if (!isLink) {
       this.closeMenu();
     }
+  }
+
+  getSkillsId(): string {
+    return window.innerWidth <= 1325 ? 'mobileSkills' : 'skills';
   }
 }
