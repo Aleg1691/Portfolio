00b403aa634e2af56fde6d59c9f94c4dd7cf06a4
Oleg Wenner <aleg1691@gmail.com>
2024-11-14 13:19:27 +0100

    Responsive

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ImprintComponent } from './imprint/imprint.component';
 import { MaincontentComponent } from './main/maincontent/maincontent.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
     { path: '', component: MaincontentComponent },
     { path: 'imprint', component: ImprintComponent },
+    { path: 'privacy-policy', component: PrivacyPolicyComponent },
 ];
